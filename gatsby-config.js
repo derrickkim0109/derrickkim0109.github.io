061b49867ee68f5a1cc21e062e7d0deb41b32564
derrickkim0109 <derrickkim0109@naver.com>
2022-04-21 19:28:26 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby new my-blog-starter https://github.com/JaeYeopHan/gatsby-starter-bee`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
